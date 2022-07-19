8eb76c4bdb18bf68c0f0eae2c37c1ae3cda29135
VanHoa18042000 <nguyen.van.hoa@sun-asterisk.com>
2022-07-19 13:02:28 +0700

    chapter6

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true,
+    length: {minimum: Settings.user.email_min, maximum: Settings.user.email_max},
+    format: {with: Settings.user.email_regex},
+    uniqueness: {case_sensitive: false}
+  validates :password, presence: true,
+    length: {minimum: Settings.user.password_min}
+
+  has_secure_password
+
+  private
+  def downcase_email
+    email.downcase!
+  end
+end
